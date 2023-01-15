c862cf54e005272d0f007837662a5d69cb3be23c
m.bayzhigit <m.bayzhigit@alterapp.net>
2023-01-15 16:34:34 +0600

    version 2.0

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use('/*', function (req, res) {
     res.sendFile(path.join(__dirname + '/frontend/index.html'))
 })
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 80
 
 moongose.set('strictQuery', true);
 
