c3da38fbf3f67af750b128649f1c952d810cbe7e
Miras010 <mirasapc@gmail.com>
2023-04-10 16:07:33 +0600

    login by phone number

--- models/User.js
+++ models/User.js
@@ -4,7 +4,7 @@ const User = new Schema({
     username: {type: String, unique: true, required: true},
     name: {type: String, required: true},
     surname: {type: String, required: true},
-    phoneNumber: {type: String, required: true},
+    phoneNumber: {type: String, unique: true, required: true},
     mail: {type: String, required: true},
     password: {type: String, required: true},
     roles: [{type: String, ref: 'Role'}],
